4e3974e3cb30e69168875c4eb7cb1669fbe900e2
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-12-26 02:07:26 -0800

    flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+A Flask application that displays 'Hello HBNB!' on the root route
+and 'HBNB' on the '/hbnb' route.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route that returns 'Hello HBNB!'.
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    Route that returns 'HBNB'.
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
