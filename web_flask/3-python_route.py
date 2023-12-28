5cfcfe5af87125d21392c4ae09b4ebe332229a17
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-12-28 03:50:45 -0800

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 A Flask application that displays different messages based on routes.
 """
@@ -25,13 +26,26 @@ def display_hbnb():
     return 'HBNB'
 
 
+@app.route('/c/', strict_slashes=False)
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
+def display_c(text='is_cool'):
     """
     Route that returns 'C ' followed by the value of the text variable.
     Underscores in the text are replaced with spaces.
+    Default value of text is 'is_cool'.
     """
     return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is_cool'):
+    """
+    Route that returns 'Python ' followed by the value of the text variable.
+    Underscores in the text are replaced with spaces.
+    Default value of text is 'is_cool'.
+    """
+    return 'Python {}'.format(escape(text).replace('_', ' '))
 
 
 if __name__ == '__main__':
